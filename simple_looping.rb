402d625240cfa9b2c7d6cb972567b8c9910b0ad9
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-25 17:40:51 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,8 +21,7 @@ def while_iterator(number_of_times)
   while counter < 7
   puts "Welcome to Flatiron School's Web Development Course!"
   counter+=1
-end
-  
+  end
 end
 
 def until_iterator(number_of_times)
