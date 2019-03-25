b62624fe72982b445758c0888ff60827819adc37
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-25 17:38:58 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,7 @@ def loop_iterator(number_of_times)
   counter = 0
   loop do
     counter+=1
-  puts = "Welcome to Flatiron School's Web Development Course!"
+  puts "Welcome to Flatiron School's Web Development Course!"
   if counter >= 7
     break
     end
